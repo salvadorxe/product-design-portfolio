1498e55f4ac310a91086e64765b4d760215fbb54
Salvador Xe <salvadorxe@salvadors-mbp.lan>
2023-08-23 19:59:23 -0400

    updated project card component & expanded about me section

--- components/About.js
+++ components/About.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import image from '../public/assets/me.png'
 
 const About = () => {
   return (
-    <section className='mt-32 w-full bg-[#2C2C2C] text-main-white'>
-      About
+    <section className='pt-12 pb-12'>
+      <div className='block md:flex flex-row justify-center space-x-12'>
+        <div className='hidden md:block basis-1/8'>
+          <img className='w-60 h-full rounded-[50%]' src={'/assets/me.png'}/>
+        </div>
+        <div className='basis-1/8'>
+          <h1 className='font-bold text-4xl pb-4'>About</h1>
+          <span className='align-middle text-lg whitespace-pre-line'>Hello there!{'\n'}I'm Salvador Xe, a Latino Product Designer {'\n'}specializing in UI & UX design. I am currently {'\n'} a candidate for a Bachelor of Arts in Computer Science, {'\n'}graduating in May 2024.{'\n\n'}<p className='font-semibold'>Creative Designs, purposeful choices.</p></span>
+          <p>Test</p>
+        </div>
+      </div>
     </section>
   )
 }
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import projData from '../public/assets/projects/proj-details.json'
+import { FaArrowRight } from "react-icons/fa6";
 
 const ProjectCard = () => {
   const projs = projData["projects"];
 
   return(
-    <div className='flex'>
+    <div className='flex flex-wrap'>
       {projs.map((proj) => (
         <div key={proj.id} className='card_container'>
-        <div className='card_body'>
-        <img className='card_cover' src={proj.img} />
-        <div className='card_text border-t-2'>
-          <h1 className='nav_text pt-2 pb-2'>{proj.title}</h1>
-          <p className='desc pb-2'>{proj.desc}</p>
-          <a className='text-main-black font-bold' href='#'>View</a>
-       </div>
+        <div>
+          <img src={proj.img} />
+          <div className='card_text border-t-2'>
+            <h1 className='nav_text pt-2 pb-2'>{proj.title}</h1>
+            <p className='desc pb-2'>{proj.desc}</p>
+              <div className='flex cursor-pointer group items-center space-x-2'>
+                <a className='text-main-black font-bold' href='#'>View </a>
+                <FaArrowRight />
+              </div>
+        </div>
       </div>
       </div>
       ))}
